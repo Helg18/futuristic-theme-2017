e621cad4f05a06bdb6a7b2fe34865b8a0ba2825c
Henry Leon <Helg18@gmail.com>
2017-05-06 22:21:40 -0400

    Commit inicial - Tema viajero

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Futuristic - theme 2017</title>
+	<meta name="viewport" content="width=device.width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Architects+Daughter|Courgette" rel="stylesheet">
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<div class="main">
+		<header id="header" class="header">
+			<div class="menu margen-interno">
+				<div class="logo">
+					<a href="index.php">LOGO</i></a>
+				</div>
+				<div class="nav">
+					<a href="index.php"><i class="fa fa-home" aria-hidden="true"></i><span class="texto-nav">Inicio</span></a>
+					<a href="index.php"><i class="fa fa-user" aria-hidden="true"></i><span class="texto-nav">Nosotros</span></a>
+					<a href="index.php"><i class="fa fa-th-large" aria-hidden="true"></i><span class="texto-nav">Productos</span></a>
+					<a href="index.php"><i class="fa fa-server" aria-hidden="true"></i><span class="texto-nav">Servicios</span></a>
+					<a href="index.php"><i class="fa fa-envelope" aria-hidden="true"></i><span class="texto-nav">Contactos</span></a>
+				</div>
+				<div class="social">
+						<div class="social-hijo">
+							<a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+						</div>
+						<div class="social-hijo">
+							<a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+						</div>
+						<div class="social-hijo">
+							<a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
+						</div>
+				</div>
+			</div>
+			<div class="texto-principal margen-interno">
+				<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti beatae accusamus vero dicta repellat cum, hic quidem maxime. Odit officiis, deleniti nulla, ea non excepturi maxime amet itaque eos aut minus, quasi quaerat illo iure tenetur adipisci accusantium quod id temporibus aliquid duc imus officia. Rerum inventore ullam, alias non eos.</h1>
+			</div>
+		</header><!-- /header -->
+		<section class="section margen-interno">
+			<div class="articulos">
+				<article class="article">
+					<img src="img/img3.jpg" alt="texto-de-ayuda">
+					<h4>Sabado, 06 de Mayo de 2017</h4>
+					<h2>Titulo de la entrada</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus commodi esse nostrum obcaecati tenetur delectus corporis nam dolorum. Ex, repudiandae? Voluptatibus commodi esse nostrum obcaecati tenetur delectus corporis nam dolorum. Ex, repudiandae? </p>
+					<a href="#">Leer mas...</a>
+				</article>
+				<article class="article">
+					<img src="img/img3.jpg" alt="texto-de-ayuda">
+					<h4>Sabado, 06 de Mayo de 2017</h4>
+					<h2>Titulo de la entrada</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus commodi esse nostrum obcaecati tenetur delectus corporis nam dolorum. Ex, repudiandae? Voluptatibus commodi esse nostrum obcaecati tenetur delectus corporis nam dolorum. Ex, repudiandae? </p>
+					<a href="#">Leer mas...</a>
+				</article>
+				<article class="article">
+					<img src="img/img3.jpg" alt="texto-de-ayuda">
+					<h4>Sabado, 06 de Mayo de 2017</h4>
+					<h2>Titulo de la entrada</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus commodi esse nostrum obcaecati tenetur delectus corporis nam dolorum. Ex, repudiandae? Voluptatibus commodi esse nostrum obcaecati tenetur delectus corporis nam dolorum. Ex, repudiandae? </p>
+					<a href="#">Leer mas...</a>
+				</article>
+				<article class="article">
+					<img src="img/img3.jpg" alt="texto-de-ayuda">
+					<h4>Sabado, 06 de Mayo de 2017</h4>
+					<h2>Titulo de la entrada</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus commodi esse nostrum obcaecati tenetur delectus corporis nam dolorum. Ex, repudiandae? Voluptatibus commodi esse nostrum obcaecati tenetur delectus corporis nam dolorum. Ex, repudiandae? </p>
+					<a href="#">Leer mas...</a>
+				</article>
+				<nav class="navegacion">
+					<a href="#">Inicio</a>
+					<a href="#">1</a>
+					<a href="#">2</a>
+					<a href="#">3</a>
+					<a href="#">4</a>
+					<a href="#">5</a>
+					<a href="#">6</a>
+					<a href="#">...</a>
+					<a href="#">Ultimo</a>
+				</nav>
+			</div>
+			<div class="aside">
+				<div class="publicidad">
+					<h4>Publicidad</h4>
+					<img src="img/conoce_venezuela.jpg">
+				</div>
+				<div class="publicidad">
+					<h4>Publicidad</h4>
+					<img src="img/conoce_venezuela.jpg">
+				</div>
+				<div class="publicidad">
+					<h4>Publicidad</h4>
+					<img src="img/conoce_venezuela.jpg">
+				</div>
+			</div>
+		</section>
+		<footer class="footer margen-interno">
+			<nav class="pie">
+				Desarrollado por <a href="mailto:helg18@gmail.com?subject=Tema-futuristic" "email me">Henry Leon</a>
+			</nav>
+		</footer>
+	</div>
+</body>
+</html>
